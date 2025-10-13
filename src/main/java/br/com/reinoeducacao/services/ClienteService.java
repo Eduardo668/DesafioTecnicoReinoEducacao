a791427ea29197d64b94f352619f68b0b519a76a
Eduardo668 <eduardosehn20@gmail.com>
2025-10-13 16:05:30 -0300

    FEAT:Adiciona a interface ClientService com os metodos bases para a regra de negocio do sistema

---
+++ src/main/java/br/com/reinoeducacao/services/ClienteService.java
@@ -0,0 +1,18 @@
+package br.com.reinoeducacao.services;
+
+import br.com.reinoeducacao.dto.ClienteDto;
+import br.com.reinoeducacao.dto.UpdateClienteDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+public interface ClienteService {
+
+    ClienteDto create(ClienteDto clienteDto);
+    List<ClienteDto> findAll();
+    ClienteDto update(Long id, UpdateClienteDto updateClienteDto);
+    void delete(Long id);
+    ClienteDto findById(Long id);
+    ClienteDto addMiles(Long id, Long miles);
+
+}
