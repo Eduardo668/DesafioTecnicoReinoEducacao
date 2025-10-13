5178736b825639978de81d23c80a157996604fe7
Eduardo668 <eduardosehn20@gmail.com>
2025-10-13 15:10:13 -0300

    START: Estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,9 @@
+# 🧠 Teste Técnico - Reino Educação
+**Desenvolvedor Backend Junior**
+
+## 📋 Descrição
+
+Este repositório contém o **Teste Técnico** proposto pela **Reino Educação** para a vaga de **Desenvolvedor Backend**.  
+O objetivo deste teste é avaliar o domínio de boas práticas, arquitetura de APIs, persistência de dados e organização do código.
+
+
---
+++ src/main/java/br/com/reinoeducacao/MileManagementReinoEduApplication.java
@@ -0,0 +1,13 @@
+package br.com.reinoeducacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileManagementReinoEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MileManagementReinoEduApplication.class, args);
+	}
+
+}
