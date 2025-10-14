c61eb909578b1af46021862ca3f1254cf75ddbfc
Eduardo668 <eduardosehn20@gmail.com>
2025-10-14 20:22:38 -0300

    UPDATE: Adiciona anotações @NoArgsContructor e @AllArgsConstructor para gerar construtores no (ClienteDto)

--- src/main/java/br/com/reinoeducacao/dtos/ClienteDto.java
+++ src/main/java/br/com/reinoeducacao/dtos/ClienteDto.java
@@ -4,13 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClienteDto {
 
     private Long id;
