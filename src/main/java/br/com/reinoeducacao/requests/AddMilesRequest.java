e0c136ba400d6c6a2895d5bb768129c47eb32730
Eduardo668 <eduardosehn20@gmail.com>
2025-10-15 17:29:37 -0300

    FEAT: Adiciona a classe AddMilesRequest que serve de modelo para o body da requisição que adiciona milhas ao cliente

---
+++ src/main/java/br/com/reinoeducacao/requests/AddMilesRequest.java
@@ -0,0 +1,14 @@
+package br.com.reinoeducacao.requests;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddMilesRequest {
+
+    @NotNull
+    private Long quantidade;
+
+}
